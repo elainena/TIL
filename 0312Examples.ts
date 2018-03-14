b8ff3b646c21f2d4718f89289bd4a57d49bc7b55
elainena <elainena93@gmail.com>
2018-03-14 10:27:01 +0900

    Add Lecture examples

---
+++ 0312Examples.ts
@@ -0,0 +1,19 @@
+//3.1 Interpolation 
+{{ example }}
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template:
+})
+
+export class AppComponent {
+    name = 'Angular';
+    age = 20;
+    admin = true;
+    address = {
+        city : 'Seoul',
+        country : 'Korea'
+    };
+}
